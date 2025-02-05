77de309e2c5ce84140a2b88d58aba8604ca2b219
DiansSopandi <dian.sopandi@gmail.com>
2025-02-05 14:25:03 +0700

    update index.ts and vercel.json

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express ,{ Request, Response, NextFunction } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import { specs, swaggerUi  } from "../swagger"
 import 'reflect-metadata';
 import bodyParser from "body-parser";
